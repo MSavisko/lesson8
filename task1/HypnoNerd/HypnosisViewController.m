b935be16130668aa8dcc933afaa49b36eded191c
Maksym Savisko <stowyn@gmail.com>
2015-12-19 16:15:42 +0200

    Finished Task #1.

--- task1/HypnoNerd/HypnosisViewController.m
+++ task1/HypnoNerd/HypnosisViewController.m
@@ -9,6 +9,9 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
+@interface HypnosisViewController () <UITextFieldDelegate>
+@end
+
 @implementation HypnosisViewController
 
 - (void)viewDidLoad {
@@ -31,6 +34,74 @@
 - (void) loadView {
     HypnosisView * backgroundView = [[HypnosisView alloc]init];
     self.view = backgroundView;
+    
+    //Add a TextField
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc]initWithFrame:textFieldRect];
+    
+    //Set th border style (Устанавливаем границу стиля текстового поля).
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    
+    //Warning
+    textField.delegate = self;
+    
+    [backgroundView addSubview:textField];
+    
+    self.view = backgroundView;
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self drawHypnoticMessage:textField.text];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void) drawHypnoticMessage:(NSString *)message {
+    for (int i=0; i < 20; i++) {
+        UILabel * messageLabel = [[UILabel alloc]init];
+        
+        // Configure Label. color's and text
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor grayColor];
+        messageLabel.text = message;
+        
+        //Resize label for text in it
+        [messageLabel sizeToFit];
+        
+        //Get a random position of x
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+        
+        //Get a random position of x
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+        
+        //Update label frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        //Add Label to Hierarchy
+        [self.view addSubview:messageLabel];
+        
+        UIInterpolatingMotionEffect *mottionEffect = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.x"
+                                                                                                    type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        mottionEffect.minimumRelativeValue = @(-25);
+        mottionEffect.maximumRelativeValue = @(25);
+        [messageLabel addMotionEffect:mottionEffect];
+        
+        mottionEffect = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.y"
+                                                                       type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        mottionEffect.minimumRelativeValue = @(-25);
+        mottionEffect.maximumRelativeValue = @(25);
+        [messageLabel addMotionEffect:mottionEffect];
+        
+    }
 }
 
 @end
